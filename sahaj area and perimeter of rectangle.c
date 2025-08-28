2a10558eaa25152a0dd4c0c38ccc0e7705eb405c
Sahaj Mishra <sahajmishra416@gmail.com>
2025-08-28 11:52:32 +0530

    Add files via upload

---
+++ sahaj area and perimeter of rectangle.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+void main()
+{
+int l,b,p,a;
+printf ("enter the lenght of the rectangle ");
+scanf("%d",&l);
+printf("enter the breadth of rectangle");
+scanf ("%d",&b);
+p=2*(l+b);
+printf("the perimeter of rectangle of sides %d% d=%d\n",l,b,p);
+a=l*b;
+printf("the area of rectangle of the given sides %d %d=%d\n",l,b,a);
+
+return 0;
+
+}
